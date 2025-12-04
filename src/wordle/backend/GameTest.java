d3a2dbf26192a5ffd3b41a5e1c002630e90694af
Felicia Pace <feliciapace@MacBook-Pro-66.local>
2025-12-04 16:10:19 -0500

    GameTest.java

---
+++ src/wordle/backend/GameTest.java
@@ -0,0 +1,62 @@
+package backend;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void guessTooShort() {
+
+    }
+
+    @Test
+    void guessTooLong() {
+
+    }
+
+    @Test
+    void guessNotValidWord() {
+
+    }
+    
+    @Test
+    void normalToHighContrast() {
+
+    }
+    
+    @Test
+    void highContrastToNormal() {
+
+    }
+
+    @Test
+    void allLettersIncorrect() {
+
+    }
+
+    @Test
+    void someYellow() {
+
+    }
+
+    @Test
+    void mixYellowAndGray() {
+
+    }
+
+    void mixGreenAndGray() {
+
+    }
+
+    void mixGreenAndYellow() {
+
+    }
+
+    @Test
+    void allLettersCorrect() {
+
+    }
+
+}
