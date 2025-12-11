face4f54574ded1e216a0421652350523dfbeb69
Ly Tran <lyktran05@gmail.com>
2025-12-10 23:40:40 -0500

    reorganized tests into a new package

--- src/wordle/testing/GameTest.java
+++ src/wordle/testing/GameTest.java
@@ -1,4 +1,4 @@
-package backend;
+package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,6 +6,7 @@ import java.awt.Color;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+import backend.*;
 
 class GameTest {
     // colors for default mode
@@ -89,5 +90,7 @@ class GameTest {
             assertEquals(color, GREEN);
         }
     }
+    
+    
 
 }
